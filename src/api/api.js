234b4111ef0225e5d72ef133c99b300f2045a080
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-31 11:05:39 +0200

    Check if we have user or catch the errors.

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,7 @@
+import { getUserData } from "../util.js";
+
+const host = 'http://localhost:3030';
+
 async function request(method, url, data) {
     const options = {
         method,
@@ -7,5 +11,31 @@ async function request(method, url, data) {
     if (data !== undefined) {
         options.headers['Content-Type'] = 'application/json';
         options.body = JSON.stringify(data);
+    }
+
+    const user = getUserData();
+
+    if (user) {
+        options.headers['X-Authorization'] = user.accessToken;
+    }
+
+    try {
+        const response = await fetch(host + url, options);
+
+        if (response.status == 204) {
+            return response;
+        }
+
+        const result = await response.json();
+
+        if (response.ok == false) {
+            throw new Error(result.message);
+        }
+
+        return result;
+
+    } catch (err) {
+        alert(err.message);
+        throw err;
     }
 }
