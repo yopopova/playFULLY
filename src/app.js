e56c6cf2977054e0e21b77113ec00cd25d458a7e
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-02 09:52:14 +0200

    Create render function to decorate app ctx.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import { render } from "../node_modules/lit-html/lit-html.js";
 import page from '../node_modules/page/page.mjs';
 
+import { showHome } from "./views/home.js";
+
+const main = document.getElementById('main-content');
+
+page(decorateContext);
 page('/index.html', '/');
-page('/', () => console.log('home'));
+page('/', showHome);
+
 page('/catalog', () => console.log('catalog'));
 page('/catalog/:id', () => console.log('details'));
 page('/edit/:id', () => console.log('edit'));
@@ -12,3 +18,13 @@ page('/login', () => console.log('login'));
 page('/register', () => console.log('register'));
 
 page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+
+    next();
+}
+
+function renderMain(content) {
+    render(content, main);
+}
