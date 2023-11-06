c2b728ccee65186ea12b372122ad6ac6360d7874
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-06 08:49:47 +0200

    Make catalog data dinamic.

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,7 +1,7 @@
 import { html, nothing } from "../../node_modules/lit-html/lit-html.js";
 import { getAll } from "../api/data.js";
 
-const catalogTemplate = (albums) => html`
+const catalogTemplate = (albums, hasUser) => html`
     <section id="catalogPage">
         ${albums.length === 0 ? html`
             <p id="no-albums">No albums in catalog</p>` : 
@@ -13,15 +13,15 @@ const albumCardTemplate = (album, hasUser) => html`
         <img src=${album.imgUrl}>
         <div>
             <div class="album-info">
-                <p class="name"><span>Name: </span>Melodrama</p>
-                <p class="artist"><span>Artist: </span>Lorde</p>
-                <p class="genre"><span>Genre: </span>Pop Music</p>
-                <p class="price"><span>Price: </span>$7.33</p>
-                <p class="date"><span>Release Date: </span>June 16, 2017</p>
+                <p class="name"><span>Name: </span>${album.name}</p>
+                <p class="artist"><span>Artist: </span>${album.artist}</p>
+                <p class="genre"><span>Genre: </span>${album.genre}</p>
+                <p class="price"><span>Price: </span>$${album.price}</p>
+                <p class="date"><span>Release Date: </span>${album.releaseDate}</p>
             </div>
 
             ${hasUser ? html`
-                <div class="btn-group">
+                <div class="details-btn">
                     <a href="/details/${album._id}" id="details">Details</a>
                 </div>`
             : nothing}
@@ -30,5 +30,5 @@ const albumCardTemplate = (album, hasUser) => html`
 
 export async function showCatalog(ctx) {
     const albums = await getAll();
-    ctx.render(catalogTemplate([]));
+    ctx.render(catalogTemplate(albums, !!ctx.user));
 }
