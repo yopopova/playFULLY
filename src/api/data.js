bfe010b6f28a51ce34473bb2de329e0507fd889a
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-08 04:37:27 +0200

    Add endpoints for create and edit album.

--- src/api/data.js
+++ src/api/data.js
@@ -11,3 +11,11 @@ export async function getById(id) {
 export async function deleteById(id) {
     return del('/data/albums/' + id);
 }
+
+export async function createAlbum(albumData) {
+    return post('/data/albums', albumData);
+}
+
+export async function editAlbum(id, albumData) {
+    return put('/data/albums/' + id, albumData);
+}
